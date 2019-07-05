e043635504ade3033cefb4c82cf960f640e16b55
MhmdAwad <mhmdawaddd@gmail.com>
2019-07-05 04:15:51 +0200

    helloWorld!

--- app/src/main/java/com/mhmdawad/inventoryapp/ViewActivity.java
+++ app/src/main/java/com/mhmdawad/inventoryapp/ViewActivity.java
@@ -31,7 +31,7 @@ public class ViewActivity extends AppCompatActivity implements LoaderManager.Loa
     phoneCursorAdapter phoneCursorAdapter;
     static final int LOADER_INT = 0;
     TextView quantityTextView;
-int aa = 110;
+int aa = 11000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
